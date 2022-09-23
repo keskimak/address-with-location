507a18d208f9332f7284017316295df6e4eb7a37
Noora Keskimäki <noora.keskimaki@gmail.com>
2022-09-23 13:35:11 +0300

    Initial location working

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
+import {
+  StyleSheet, Text, View, TextInput, Button, Dimensions, TouchableOpacity, Alert,
+  TouchableWithoutFeedback, FlatList, Keyboard, KeyboardAvoidingView, Platform
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [location, setLocation] = useState(null);
+  const [coords, setCoords] = useState({latitude: null, longitude: null});
+ 
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+       if (status !== 'granted') {
+         Alert.alert('No permission to get location')
+         return; }
+
+        let location = await Location.getCurrentPositionAsync({});    
+        setLocation(location);
+        setCoords({latitude: location.coords.latitude, longitude: location.coords.longitude})
+      
+        console.log(location)
+        console.log(location.coords.latitude, location.coords.longitude)
+       
+        })();
+      }, []);
+
+
+      return (
+        <View style={styles.container}>
+          <MapView
+          region={ { latitude: coords.latitude, longitude: coords.longitude, latitudeDelta: 0.0322,longitudeDelta: 0.0221} }
+           
+            width={styles.map.width}
+            height={styles.map.height}
+          >
+            <Marker
+            coordinate={ { latitude: 60.200692,longitude: 24.934302}}
+            title='Title'
+            />
+          
+
+
+          </MapView>
+
+
+        </View>
+
+
+      )
+    };
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      map: {
+        width: Dimensions.get('window').width,
+        height: (Dimensions.get('window').height) * 0.91,
+        marginBottom: 10
+      },
+    });
